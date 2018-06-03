8c87c248bb539cd072990bd423c715044ac0ce4c
Andrii <38627095+AndriiChemerys@users.noreply.github.com>
2018-06-03 16:45:51 +0200

    adding buttons with js

--- git_js_dom_5/w01/app.js
+++ git_js_dom_5/w01/app.js
@@ -6,7 +6,26 @@ const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector("ul");
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const lis=listUl.children;
 
+function attachListItemButtons(li) {
+    let up=document.createElement('button');
+    up.className="up";
+    up.textContent="up";
+    li.appendChild(up);
+    let down=document.createElement('button');
+    down.className="down";
+    down.textContent="down";
+    li.appendChild(down);
+    let remove=document.createElement('button');
+    remove.className="remove";
+    remove.textContent="remove";
+    li.appendChild(remove);
+}
+
+for (let i = 0; i< lis.length; i+=1) {
+    attachListItemButtons(lis[i]);
+}
 
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
@@ -52,10 +71,12 @@ addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  attachListItemButtons(li);
   ul.appendChild(li);
   addItemInput.value = '';
 });
   
+
 
   
   
