874e5e28d5ef9447af9b807d181a0ec36e094443
AndriiChemerys <andriichemerys@gmail.com>
2019-11-10 16:05:53 +0100

    alphabetic shift

--- 6_js_100_algorithms_challenge/8_alphabeticShift/alphabeticShift.ts
+++ 6_js_100_algorithms_challenge/8_alphabeticShift/alphabeticShift.ts
@@ -2,7 +2,7 @@ function alphabeticShift(inputString: string): string {
     const alphabet: string[]=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
     let inputShifted = inputString.split('');
-
+    console.log(inputShifted);
     for(let i=0; i<inputShifted.length; i++){
         let index=0;
         if(inputShifted[i]!=='z'){
