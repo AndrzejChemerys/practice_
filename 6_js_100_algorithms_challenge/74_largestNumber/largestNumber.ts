fb992b356efee843c9dcb9cf8be98b939f05d004
AndriiChemerys <andriichemerys@gmail.com>
2019-12-08 21:04:11 +0100

    largest number

---
+++ 6_js_100_algorithms_challenge/74_largestNumber/largestNumber.ts
@@ -0,0 +1,13 @@
+function largestNumber(n: number): number {
+//  let word = "";
+
+//  for(let i=0; i<n; i++){
+//      word=word.concat('9');
+//  }
+
+let word='9'.repeat(n);
+
+ return parseInt(word);
+}
+
+console.log(largestNumber(2));
