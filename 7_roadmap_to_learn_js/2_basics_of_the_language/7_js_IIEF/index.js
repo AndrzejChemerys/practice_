291f10000519494e2690be8f8c541250265c5d6b
AndriiChemerys <andriichemerys@gmail.com>
2019-12-18 20:15:32 +0100

    js func

---
+++ 7_roadmap_to_learn_js/2_basics_of_the_language/7_js_IIEF/index.js
@@ -0,0 +1,35 @@
+// JavaScript Immediately-invoked Function Expressions (IIFE)\
+
+console.log(
+(function() {
+    return 2+3;
+})()
+)
+
+console.log(
+    (() => {
+        return 1+3
+      })()
+)
+
+console.log(
+    (function() {
+        return 2+6
+      }())
+)
+
+// console.log(
+//     (() => {
+//         return 3
+//       }())
+// )
+
+console.log(
+    ~(function() {
+        return 2+2
+      })()
+      )
+
+
+
+
