8ec5385e1b20f9ec345c23c64cee0c65808dd93d
AndriiChemerys <andriichemerys@gmail.com>
2020-01-10 22:13:30 +0100

    first digit

--- 6_js_100_algorithms_challenge/60_firstDigit/firstDigit.ts
+++ 6_js_100_algorithms_challenge/60_firstDigit/firstDigit.ts
@@ -10,5 +10,5 @@ function firstDigit(inputString: string): string {
 }
 
 console.log(firstDigit('var_1__Int'));
-console.log(firstDigit('q2q-q'));
+console.log(firstDigit('4q12q-q3'));
 console.log(firstDigit('0ss'));
