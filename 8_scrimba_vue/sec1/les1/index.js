efd4acb19837c98b3c841d19af53c2a746e513a6
AndriiChemerys <andriichemerys@gmail.com>
2020-04-23 21:16:44 +0200

    first steps with vue

--- 8_scrimba_vue/sec1/les1/index.js
+++ 8_scrimba_vue/sec1/les1/index.js
@@ -1,7 +1,26 @@
-var app = new Vue({ 
+var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue !!!',
-        seen: true
+        seen: true,
+        todos: [{
+                text: 'Learn JavaScript'
+            },
+            {
+                text: 'Learn Vue'
+            },
+            {
+                text: 'Build something awesome'
+            }
+        ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 });
+
+app.todos.push({
+    text: 'New item'
+})
