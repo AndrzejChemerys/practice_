dbb531720f46d0f822c3593a631ae11b1a256212
AndriiChemerys <andriichemerys@gmail.com>
2020-07-03 20:26:07 +0200

    Make Array Consecutive 2

--- 6_js_100_algorithms_challenge/77_longestDigitsPrefix/longestDigitsPrefix.ts
+++ 6_js_100_algorithms_challenge/77_longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,18 @@
 function longestDigitsPrefix(inputString: string): string {
+  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const chars = inputString.split("");
+  let prefix = "";
 
+  for (let char of inputString) {
+    const num = parseInt(char);
+
+    if (isNaN(num)) {
+      break;
+    }
+    prefix += char;
+  }
+  //   return prefix.join("");
+  return prefix;
 }
 
-console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix("123aa1"));
