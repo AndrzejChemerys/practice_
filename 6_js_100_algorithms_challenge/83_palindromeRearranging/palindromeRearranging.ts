86579487b8b9a20b3f1678bd3423a6a77353ac50
AndriiChemerys <andriichemerys@gmail.com>
2020-08-27 21:15:44 +0200

    Palindrome Rearranging

--- 6_js_100_algorithms_challenge/83_palindromeRearranging/palindromeRearranging.ts
+++ 6_js_100_algorithms_challenge/83_palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,23 @@
 function palindromeRearranging(inputString: string): boolean {
+  const chars = inputString.split("");
+  const charCount = {};
+  let oddCount = 0;
 
+  for (let i = 0; i < chars.length; i++) {
+    if (charCount.hasOwnProperty(chars[i])) {
+      charCount[chars[i]]++;
+    } else {
+      charCount[chars[i]] = 1;
+    }
+  }
+
+  for (let key in charCount) {
+    if (charCount[key] % 2 !== 0) {
+      oddCount++;
+    }
+  }
+
+  return oddCount > 1 ? false : true;
 }
 
-console.log(palindromeRearranging('aabb'));
+console.log(palindromeRearranging("aabb"));
